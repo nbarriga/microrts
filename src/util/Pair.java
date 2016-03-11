0bc6e02998d4d5bb3b5aaaf650aee45da2e7b68c
Nicolas A. Barriga <nbarriga@gmail.com>
2016-03-11 16:13:34 -0700

    Added equals method to Pair.
    Probably should add hashCode too.

--- src/util/Pair.java
+++ src/util/Pair.java
@@ -5,13 +5,20 @@ package util;
 public class Pair<T1,T2> {
 	public T1 m_a;
 	public T2 m_b;
-	
+
 	public Pair(T1 a,T2 b) {
 		m_a = a;
 		m_b = b;
 	}   
-        
-        public String toString() {
-            return "<" + m_a + "," + m_b + ">";
-        }
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Pair<?,?>)){
+			return false;
+		}
+		Pair<T1,T2> p=(Pair<T1,T2>)obj;
+		return m_a.equals(p.m_a)&&m_b.equals(p.m_b);
+	}
+	public String toString() {
+		return "<" + m_a + "," + m_b + ">";
+	}
 }
